ec8cde0ef4ddb85c481566717e9841b8bb6b201f
Louis Piochaud <louis.piochaud@epitech.eu>
2024-02-04 08:44:26 -0800

    feat!: adding the tests to the template

--- src/template.rs
+++ src/template.rs
@@ -1,4 +1,4 @@
-use crate::leetcode_problem::LeetCodeProlem;
+use crate::leetcode_problem::LeetCodeProblem;
 
 pub fn cargo_template(package_name: &str) -> String
 {
@@ -69,14 +69,17 @@ pub fn main_rs_base_template() -> String
     return format!(
         "pub struct Solution;
     
-    {{code}}
+{{code}}
+    
+{{tests}}
 
 {{main_function}}
+
 "
     );
 }
 
-fn generate_vardecla(pb: LeetCodeProlem) -> String
+fn _generate_vardecla(pb: LeetCodeProblem) -> String
 {
     let result: String = pb
         .init_var
@@ -90,14 +93,57 @@ fn generate_vardecla(pb: LeetCodeProlem) -> String
     return result;
 }
 
-pub fn main_function_template(pb: LeetCodeProlem) -> String
+pub fn tests_template(pb: LeetCodeProblem) -> String
+{
+    let mut r = format!(
+        r#"
+#[cfg(test)]
+mod tests {{
+    use super::*;
+"#,
+    );
+    for i in 0..pb.total_examples.unwrap() {
+        r += &format!(
+            r#"
+    #[test]
+    fn test_example_{}() {{"#,
+            i + 1
+        );
+
+        {
+            r += &format!(
+                r#"
+        let {}:{} = {};"#,
+                pb.init_var.as_ref().unwrap()[i as usize].0,
+                pb.init_var.as_ref().unwrap()[i as usize].1,
+                pb.init_var.as_ref().unwrap()[i as usize].2
+            );
+        }
+        r += &format!(
+            r#"
+        let result = Solution::{}({});
+        let expected: {} = {};
+        assert_eq!(result, expected);
+    }}
+"#,
+            pb.fn_name.as_ref().unwrap(),
+            pb.init_var.as_ref().unwrap()[i as usize].0,
+            pb.fn_rtype.as_ref().unwrap(),
+            pb.expected_result.as_ref().unwrap()[i as usize]
+        );
+    }
+    r += "}\n";
+    return r;
+}
+
+pub fn main_function_template(pb: LeetCodeProblem) -> String
 {
     let var_decla = "".to_string();
     return format!(
         "fn main() {{
     {}
-    let result = Solution::{}();
-    println!(\"{{:?}}\", result);
+    // let result = Solution::{}();
+    // println!(\"{{:?}}\", result);
 }}",
         var_decla,
         pb.fn_name.expect("fn_name is None")
