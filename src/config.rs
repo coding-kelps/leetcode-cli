2d8b556bac2c6e0ab1d6a22f01fb477741b43f61
dfayd <78728332+dfayd0@users.noreply.github.com>
2025-04-05 13:02:11 +0200

    fix: some problem using tild for home path, cleaning the code from println.

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,16 @@
 #![allow(dead_code)]
+#![allow(unreachable_code)]
+
+use std::path::PathBuf;
 
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct Config
 {
+    home_dir: Option<PathBuf>,
+    config_dir: Option<PathBuf>,
+    config_file: Option<PathBuf>,
     leetcode_token: Option<String>,
 }
 
@@ -12,11 +18,18 @@ impl Config
 {
     pub fn new() -> Self
     {
+        let home_dir = dirs::home_dir().expect("Unable to determine home directory");
+        let config_dir = home_dir.join(".config/leetcode-cli");
+        let config_file = config_dir.join("config.toml");
+
         Config {
+            home_dir: Some(home_dir),
+            config_dir: Some(config_dir),
+            config_file: Some(config_file),
             leetcode_token: None,
         }
     }
-
+    /// create a config file in ~/.config/leetcode-cli/config.toml
     fn create_config_file(&self)
     {
         let home_dir =
@@ -27,45 +40,27 @@ impl Config
             .expect("Unable to create directory");
         let config_file = config_dir.join("config.toml");
         std::fs::File::create(&config_file).expect("Unable to create file");
-        println!("Config file created at {:?}", config_file);
     }
-    /// check for a config file in
-    /// - ~/.config/leetcode-cli/config.toml
+    /// check for a config file in ~/.config/leetcode-cli/config.toml
     pub async fn status(&mut self) -> Result<(), reqwest::Error>
     {
-        println!("Checking for config file...");
-        if std::path::Path::new("~/.config/leetcode-cli/config.toml").exists() {
-            println!("Config file exists.");
-            println!("Reading config file...");
+        if self.config_file.is_some() {
             let config_file =
-                std::fs::read_to_string("~/.config/leetcode-cli/config.toml")
+                std::fs::read_to_string(self.config_file.as_ref().unwrap())
                     .expect("Unable to read file");
-            println!("Parsing config file...");
             let config: Config =
                 toml::from_str(&config_file).expect("Unable to parse file");
-            println!("Config file parsed.");
-            println!("Config file: {:?}", config);
             self.leetcode_token = config.leetcode_token;
             self.check_token().await?;
         } else {
-            println!("Config file does not exist");
-            println!("Creating default config file...");
             self.create_config_file();
             self.generate_token();
         }
         Ok(())
     }
 
-    async fn check_token(&self) -> Result<bool, reqwest::Error>
+    async fn test_token_validity(&self) -> Result<bool, reqwest::Error>
     {
-        unimplemented!();
-        let token = self.leetcode_token.as_ref();
-        if token.is_none() {
-            println!("Leetcode token not found");
-            println!("Let's set it together.");
-            self.generate_token();
-            return Ok(true);
-        }
         let token = self.leetcode_token.as_ref().unwrap();
         let url = format!(
             "https://leetcode.com/api/user/check_token/?token={}",
@@ -78,9 +73,21 @@ impl Config
         Ok(false)
     }
 
+    async fn check_token(&mut self) -> Result<bool, reqwest::Error>
+    {
+        let token = self.leetcode_token.as_ref();
+        if token.is_none() {
+            println!("Leetcode token not found");
+            println!("Let's set it together.");
+            self.generate_token();
+            return Ok(true);
+        }
+        self.test_token_validity().await
+    }
+
     fn generate_token(&mut self)
     {
-        unimplemented!();
+        unimplemented!("generating token is not implemented yet");
         println!("Generating token...");
         println!("Please visit https://leetcode.com/submissions/#/1");
         println!("and copy the token here:");
