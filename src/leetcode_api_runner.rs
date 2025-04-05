e5246f31baa5f1f6fb5f80088f49404ab8ef190a
dfayd <78728332+dfayd0@users.noreply.github.com>
2025-04-05 15:09:23 +0200

    feat(get_problem_info): cli prints some info about a given problem

--- src/leetcode_api_runner.rs
+++ src/leetcode_api_runner.rs
@@ -1,30 +1,54 @@
 #![allow(unused)]
+use colored::Colorize;
+use leetcoderustapi::{
+    resources::Description,
+    UserApi,
+};
+use nanohtml2text::html2text;
+
 use crate::config::Config;
-use leetcoderustapi::UserApi;
 
 pub struct LeetcodeApiRunner
 {
     config: Config,
-    api: UserApi,
+    api:    UserApi,
 }
 
 impl LeetcodeApiRunner
 {
-    pub async fn new(mut config: Config) -> Self {
+    pub async fn new(mut config: Config) -> Self
+    {
         let token = config.leetcode_token.take().unwrap();
         let api = UserApi::new(&token).await.unwrap();
-        LeetcodeApiRunner { config, api }
+        LeetcodeApiRunner {
+            config,
+            api,
+        }
     }
 
     pub async fn get_problem_info(&self, id: u32)
     {
-        unimplemented!("asking for problem info");
-        // Implement the logic to get problem info using self.api
+        let pb = self.api.set_problem_by_id(id).await.unwrap();
+
+        let title = pb.description().unwrap().name.bold().cyan();
+        let difficulty = match pb.difficulty().as_str() {
+            "Easy" => "Easy".green(),
+            "Medium" => "Medium".yellow(),
+            "Hard" => "Hard".red(),
+            _ => "Unknown".normal(),
+        };
+        let description = html2text(&pb.description().unwrap().content);
+
+        println!("{} {}: {}", id, difficulty, title);
+        println!("{}", description);
     }
 
     pub async fn submit_solution(&self, id: u32, path_to_file: String)
     {
         unimplemented!();
-        // Implement the logic to submit solution using self.api
+        // let subm_response = problem_info
+        // .send_subm(ProgrammingLanguage::Rust, "impl Solution { fn two_sum()
+        // {}}") .await
+        // .unwrap();
     }
 }
