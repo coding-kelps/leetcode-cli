8775017bdbf0cc752ed941cd032c4d58b4d29397
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-05-14 02:43:43 +0200

    feat(config): supporting default and custom dir path for leetcode folder

--- src/config.rs
+++ src/config.rs
@@ -1,40 +1,43 @@
 #![allow(dead_code)]
 #![allow(unreachable_code)]
 
-use std::path::PathBuf;
+use std::{
+    fs,
+    io,
+    path::PathBuf,
+};
 
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-pub struct Config
-{
-    home_dir:             Option<PathBuf>,
-    config_dir:           Option<PathBuf>,
-    config_file:          Option<PathBuf>,
-    pub leetcode_token:   Option<String>,
-    pub default_language: Option<String>,
+pub struct Config {
+    home_dir:              Option<PathBuf>,
+    config_dir:            Option<PathBuf>,
+    config_file:           Option<PathBuf>,
+    pub leetcode_token:    Option<String>,
+    pub default_language:  Option<String>,
+    pub leetcode_dir_path: Option<PathBuf>,
 }
 
-impl Config
-{
-    pub fn new() -> Self
-    {
+impl Config {
+    pub fn new() -> Self {
         let home_dir =
             dirs::home_dir().expect("Unable to determine home directory");
         let config_dir = home_dir.join(".config/leetcode-cli");
         let config_file = config_dir.join("config.toml");
+        let default_leetcode_dir = home_dir.join("leetcode");
 
         Config {
-            home_dir:         Some(home_dir),
-            config_dir:       Some(config_dir),
-            config_file:      Some(config_file),
-            leetcode_token:   None,
-            default_language: None,
+            home_dir:          Some(home_dir),
+            config_dir:        Some(config_dir),
+            config_file:       Some(config_file),
+            leetcode_token:    None,
+            default_language:  None,
+            leetcode_dir_path: Some(default_leetcode_dir),
         }
     }
     /// create a config file in ~/.config/leetcode-cli/config.toml
-    fn create_config_file(&self)
-    {
+    fn create_config_file(&self) {
         let home_dir =
             dirs::home_dir().expect("Unable to determine home directory");
         let config_dir = home_dir.join(".config/leetcode-cli");
@@ -45,8 +48,7 @@ impl Config
         std::fs::File::create(&config_file).expect("Unable to create file");
     }
     /// check for a config file in ~/.config/leetcode-cli/config.toml
-    pub async fn status(&mut self) -> Result<(), reqwest::Error>
-    {
+    pub async fn status(&mut self) -> Result<(), reqwest::Error> {
         if self.config_file.is_some() {
             let config_file =
                 std::fs::read_to_string(self.config_file.as_ref().unwrap())
@@ -55,13 +57,10 @@ impl Config
                 toml::from_str(&config_file).expect("Unable to parse file");
             self.leetcode_token = config.leetcode_token;
             self.default_language = config.default_language;
-            if self.default_language.is_some() {
-                println!(
-                    "default_language set to {} in {}",
-                    self.default_language.as_ref().unwrap(),
-                    self.config_file.as_ref().unwrap().display()
-                );
+            if let Some(custom) = config.leetcode_dir_path {
+                self.leetcode_dir_path = Some(PathBuf::from(custom));
             }
+            dbg!(&self);
             self.check_token().await?;
         } else {
             self.create_config_file();
@@ -70,8 +69,7 @@ impl Config
         Ok(())
     }
 
-    async fn test_token_validity(&self) -> Result<bool, reqwest::Error>
-    {
+    async fn test_token_validity(&self) -> Result<bool, reqwest::Error> {
         let token = self.leetcode_token.as_ref().unwrap();
         let url = format!(
             "https://leetcode.com/api/user/check_token/?token={}",
@@ -84,8 +82,7 @@ impl Config
         Ok(false)
     }
 
-    async fn check_token(&mut self) -> Result<bool, reqwest::Error>
-    {
+    async fn check_token(&mut self) -> Result<bool, reqwest::Error> {
         let token = self.leetcode_token.as_ref();
         if token.is_none() {
             println!("Leetcode token not found");
@@ -96,9 +93,11 @@ impl Config
         self.test_token_validity().await
     }
 
-    fn generate_token(&mut self)
-    {
-        unimplemented!("generating token is not implemented yet");
+    fn generate_token(&mut self) {
+        unimplemented!(
+            "generating token is not implemented yet. Refer to the README for \
+             instructions."
+        );
         println!("Generating token...");
         println!("Please visit https://leetcode.com/submissions/#/1");
         println!("and copy the token here:");
@@ -113,5 +112,38 @@ impl Config
         }
         self.leetcode_token = Some(token.to_string());
         println!("Token set to: {}", token);
+    }
+
+    /// Resolve the configured LeetCode directory, expand ~, canonicalize, and
+    /// create if missing.
+    pub fn resolve_leetcode_dir(&self) -> io::Result<PathBuf> {
+        let raw = if let Some(ref custom) = self.leetcode_dir_path {
+            custom.to_string_lossy()
+        } else {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "No leetcode_dir_path set",
+            ));
+        };
+        let raw_str = raw.as_ref();
+        let mut path = if raw_str == "~" {
+            dirs::home_dir().ok_or(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Unable to find home directory",
+            ))?
+        } else if raw_str.starts_with("~/") {
+            let home = dirs::home_dir().ok_or(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Unable to find home directory",
+            ))?;
+            home.join(&raw_str[2..])
+        } else {
+            PathBuf::from(raw_str)
+        };
+        // Canonicalize parent to normalize .. and . components
+        path = fs::canonicalize(&path).unwrap_or(path);
+        // Ensure directory exists
+        fs::create_dir_all(&path)?;
+        Ok(path)
     }
 }
