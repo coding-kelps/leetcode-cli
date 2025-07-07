ab0a29b472c8cecec6591a94795811eb1d2ad5d2
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-07-07 21:25:31 +0200

    feat(main): spinner for each command

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use leetcode_cli::{
     LeetcodeApiRunner,
     RuntimeConfigSetup,
 };
+use spinners::{
+    Spinner,
+    Spinners,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,7 +22,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Commands::Info {
             id,
         } => {
-            println!("{}", api_runner.get_problem_info(*id).await?);
+            let mut spinner = Spinner::new(
+                Spinners::Dots12,
+                "Fetching problem info...".into(),
+            );
+            let result = api_runner.get_problem_info(*id).await?;
+            spinner.stop();
+            println!("{}", result);
         },
         Commands::Start {
             id,
@@ -29,24 +39,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Some(lang) => utils::parse_programming_language(lang)?,
                 None => utils::parse_programming_language(default)?,
             };
+            let mut spinner = Spinner::new(
+                Spinners::Dots12,
+                "Starting problem setup...".into(),
+            );
             let start_problem = api_runner.start_problem(*id, lang).await?;
+            spinner.stop();
             println!("{}\n\nHappy coding :)", start_problem);
         },
         Commands::Test {
             id,
             path_to_file,
         } => {
+            let mut spinner =
+                Spinner::new(Spinners::Dots12, "Running tests...".into());
             let test_result =
                 api_runner.test_response(*id, path_to_file.clone()).await?;
+            spinner.stop();
             println!("Test result: {}", test_result);
         },
         Commands::Submit {
             id,
             path_to_file,
         } => {
+            let mut spinner =
+                Spinner::new(Spinners::Dots12, "Submitting solution...".into());
             let submit_result = api_runner
                 .submit_response(*id, path_to_file.clone())
                 .await?;
+            spinner.stop();
             println!("Submit result: {}", submit_result);
         },
     }
